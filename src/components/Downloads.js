31f1dc1376e8ed17959d593caefebad94aa260e0
Sara Sytela <sara.sytela@futurice.com>
2017-08-28 09:46:04 +0300

    Added downloads section

--- src/components/Downloads.js
+++ src/components/Downloads.js
@@ -1,12 +1,71 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { csv } from 'd3-request';
+import { NavLink } from 'react-router-dom';
+import intl from 'react-intl-universal';
+import { CircularProgress } from 'material-ui/Progress';
+import Card, { CardHeader, CardContent, CardActions } from 'material-ui/Card';
+import FileDownload from 'material-ui-icons/FileDownload';
+
+import downloads from '../data/downloads.csv';
+import './../styles/components/Downloads.css';
 
 class Downloads extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      files: [],
+      error: false,
+      loading: true,
+    };
+  }
+
+  componentWillMount() {
+    csv(downloads, (error, data) => {
+      if (error) {
+        this.setState({
+          error: true,
+        });
+      }
+
+      this.setState({
+        files: data,
+        loading: false,
+        error: false,
+      });
+    });
+  }
+
+  renderCards() {
+    return this.state.files.map((data, i) => {
+      const filepath = require(`../data/downloads/${data.filename}`);
+
+      return (
+        <Card className="card box-shadow" key={i}>
+          <a href={filepath} target="_blank" download={data.filename}>
+            <CardHeader className="header" title={data.title} />
+            <CardContent className="content">
+              {data.desc}
+            </CardContent>
+            <CardActions className="actions">
+              <FileDownload />
+            </CardActions>
+          </a>
+        </Card>
+      );
+    });
+  }
+
   render() {
     return (
-      <section>
-        <p>
-          Downloads Stuff
-        </p>
+      <section className="downloads flex-container">
+        {this.state.loading ? <CircularProgress className="loading" /> : null}
+        {this.state.error
+          ? <div className="not-found">
+              {intl.get('LOADING_ERROR')}
+            </div>
+          : null}
+        {this.state.files ? this.renderCards() : null}
       </section>
     );
   }
