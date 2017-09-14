126029f8853cbb1eeb5472ff853e1f00e3d7930d
mustafasaifee42 <mustafa.saifee@futurice.com>
2017-09-14 10:14:13 +0300

    Updating Timeline JS

--- src/components/DataTimeline.js
+++ src/components/DataTimeline.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './../styles/components/DataTimeline.css';
+import saferGlobeJson from './../data/data.json';
+import * as d3 from 'd3';
+
+const playSvg = require('./../assets/play-icon.svg');
+const pauseSvg = require('./../assets/pause-icon.svg');
 
 /*
   This component builds the timeline that sits at the bottom of the screen on the data page.
@@ -16,17 +21,19 @@ class DataTimeline extends Component {
   constructor(props) {
     super(props);
 
-    const startYear = 2005;
-    const endYear = 2016 + 1; // +1 fixes calculation to add defined year in the list too
+    const startYear = parseInt(d3.keys(saferGlobeJson[0].years)[0], 10);
+    const endYear =
+      parseInt(d3.keys(saferGlobeJson[0].years).slice(-1)[0], 10) + 1; // +1 fixes calculation to add defined year in the list too
 
     this.state = {
-      activeYear: 2016,
+      activeYear: endYear - 1,
       years: Array.from(
         {
           length: endYear - startYear,
         },
         (v, i) => startYear + i,
       ),
+      play: false,
     };
 
     this.processNewGPIYear = this.processNewGPIYear.bind(this);
@@ -34,18 +41,32 @@ class DataTimeline extends Component {
 
   processNewGPIYear(year) {
     this.setState({ activeYear: year });
+    this.setState({ play: false });
     this.props.updateGPIYear(year);
   }
 
+  processPlay(clck) {
+    this.setState({ play: clck });
+  }
   render() {
     return (
       <div className="flex-container flex-spread at-flex-end">
-        {this.state.years.map((year, i) =>
+        <div
+          className={`play-button ${this.state.play === true ? 'active-play' : ''}`}
+          onClick={() => {
+            this.processPlay(!this.state.play);
+          }}
+        >
+          <img
+            src={this.state.play === true ? pauseSvg : playSvg}
+            alt={this.state.play === true ? 'Pause Icon' : 'Play Icon'}
+          />
+        </div>
+        {this.state.years.map((year, i) => (
           <div
             key={i}
-            className={`timeline-item ${this.state.activeYear === year
-              ? 'active'
-              : ''}`}
+            className={`timeline-item ${year} ${this.state.activeYear === year ? 'active' : ''}`}
+            id={year}
             onClick={() => {
               this.processNewGPIYear(year);
             }}
@@ -53,8 +74,8 @@ class DataTimeline extends Component {
             <span className="timeline-item-year">
               {year}
             </span>
-          </div>,
-        )}
+          </div>
+        ))}
       </div>
     );
   }
