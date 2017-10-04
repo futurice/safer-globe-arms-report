d809ebc1a4a7b710dd404c9fc5958bc0d648929a
David Sheldrick <david@futurice.com>
2017-10-04 23:38:59 +0100

    fix typo in ssh port

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 rsync -r --delete-after -e 'ssh -p 10059' --quiet $TRAVIS_BUILD_DIR/build $SAFER_GLOBE_SSH_USER@$SAFER_GLOBE_DEPLOY_HOST:/tmp/armsreport
-ssh $SAFER_GLOBE_SSH_USER@$SAFER_GLOBE_DEPLOY_HOST -p 1059 <<'ENDSSH'
+ssh $SAFER_GLOBE_SSH_USER@$SAFER_GLOBE_DEPLOY_HOST -p 10059 <<'ENDSSH'
 rm -rf /data/wordpress/htdocs/armsreport
 mv /tmp/armsreport/build /data/wordpress/htdocs/armsreport
 ENDSSH
