4fed9dc8c15a619fd776eab7ce9704f832d0859b
David Sheldrick <david@futurice.com>
2017-10-05 08:49:56 +0100

    add sanity check in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rsync -r --delete-after -e 'ssh -p 10059' --quiet $TRAVIS_BUILD_DIR/build $SAFER_GLOBE_SSH_USER@$SAFER_GLOBE_DEPLOY_HOST:/tmp/armsreport
+rsync -r --delete-after -e 'ssh -p 10059' --quiet $TRAVIS_BUILD_DIR/build $SAFER_GLOBE_SSH_USER@$SAFER_GLOBE_DEPLOY_HOST:/tmp/armsreport || exit 1
 ssh $SAFER_GLOBE_SSH_USER@$SAFER_GLOBE_DEPLOY_HOST -p 10059 <<'ENDSSH'
 rm -rf /data/wordpress/htdocs/armsreport
 mv /tmp/armsreport/build /data/wordpress/htdocs/armsreport
