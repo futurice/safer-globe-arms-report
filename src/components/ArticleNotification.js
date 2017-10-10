1459fb59f6ed153f8a494cc6442891f5805d97e3
Kamil Waheed <kwah@client-2-157.guest.futurice.org>
2017-10-10 17:11:43 +0300

    Make it so the article language notification always shows

--- src/components/ArticleNotification.js
+++ src/components/ArticleNotification.js
@@ -8,28 +8,25 @@ export default class ArticleNotification extends Component {
   constructor(props) {
     super(props);
 
-    const isOpen = JSON.parse(
-      window.localStorage.getItem('showArticleNotification'),
-    );
     this.state = {
-      open: isOpen !== null ? isOpen : true,
+      open: true,
     };
   }
 
   closeNotification() {
-    window.localStorage.setItem('showArticleNotification', false);
-
     this.setState({
       open: false,
     });
   }
 
   render() {
-    return this.state.open && intl.options.currentLocale.includes('en') ? (
-      <div className="article-notification">
-        <span>{intl.get('ARTICLES_ONLY_IN_FINNISH')}</span>
-        <Clear onClick={this.closeNotification.bind(this)} />
-      </div>
-    ) : null;
+    return this.state.open && intl.options.currentLocale.includes('en')
+      ? <div className="article-notification">
+          <span>
+            {intl.get('ARTICLES_ONLY_IN_FINNISH')}
+          </span>
+          <Clear onClick={this.closeNotification.bind(this)} />
+        </div>
+      : null;
   }
 }
