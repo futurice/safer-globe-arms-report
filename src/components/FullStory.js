fac0e42d7eb95af077e9885015640defd8a9ce45
David Sheldrick <david@futurice.com>
2017-10-16 11:49:05 +0100

    fix path resolution bug

--- src/components/FullStory.js
+++ src/components/FullStory.js
@@ -16,7 +16,7 @@ const TwitterIcon = generateShareIcon('twitter');
 // This function resolves a local url agains the installation dir of the site
 // e.g. if the site is hosted at blah.com/arms/ it resolves /imgs/foo.jpg to /arms/imgs/foo.jpg
 function resolveUrl(path) {
-  return process.env.PUBLIC_URL + (path.startsWith('/') ? '' : '/' + path);
+  return process.env.PUBLIC_URL + (path.startsWith('/') ? '' : '/') + path;
 }
 
 class FullStory extends Component {
